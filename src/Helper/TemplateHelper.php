dd2935c2a08821258966bc72eaca00a0af465bc1
mudde <olaf.mudde@xs4all.nl>
2022-03-31 21:37:07 +0200

    Oops, forgotten

---
+++ src/Helper/TemplateHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mudde\Import\Helper;
+
+use Mustache_Engine;
+
+class TemplateHelper
+{
+    public static $m;
+
+    static function render($text, $data): string
+    {
+        $m = self::$m ?? new Mustache_Engine();
+
+        return $m->render($text, $data);
+    }
+}
---
+++ src/Source.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Mudde\Import;
+
+use Iterator;
+use Mudde\Import\Helper\ObjectHelper;
+
+class Source implements Iterator
+{
+    private $source;
+
+    public function __construct($config)
+    {
+        $this->source = ObjectHelper::getObject($config, 'Mudde\Import\Source\\');
+    }
+
+    public function init(){
+        $this->source->init();
+    }
+
+    public function current(): mixed
+    {
+        return $this->source->current();
+    }
+
+    public function next(): void
+    {
+        $this->source->next();
+    }
+
+    public function key(): mixed
+    {
+        return $this->source->key();
+    }
+
+    public function valid(): bool
+    {
+        return $this->source->valid();
+    }
+
+    public function rewind(): void
+    {
+        $this->source->rewind();
+    }
+
+    public function getSelector(): string
+    {
+        return $this->selector;
+    }
+
+    public function setSelector(string $selector): self
+    {
+        $this->selector = $selector;
+
+        return $this;
+    }
+}
